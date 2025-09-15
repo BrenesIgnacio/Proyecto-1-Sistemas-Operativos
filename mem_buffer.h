bf43c54eb8b67983d2892f385e81e2186970979d
Ignacio Brenes Mendez <65673677+BrenesIgnacio@users.noreply.github.com>
2025-09-15 19:34:30 -0400

    Add files via upload

---
+++ mem_buffer.h
@@ -0,0 +1,11 @@
+#ifndef __MEM_BUFFER__
+#define __MEM_BUFFER__
+#include "huffman.h"
+struct mem_region {
+    size_t cur;
+    size_t end;
+    void *data;
+};
+struct buffer_ops * create_mem_buffer_ops(void *buffer, size_t len);
+void desotry_mem_buffer_ops(struct buffer_ops *ops);
+#endif
